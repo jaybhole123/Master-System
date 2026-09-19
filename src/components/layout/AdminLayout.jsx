f20e8d5e2039276efa8b98c49be6f33a0e046088
NikhilNishad06 <nikhilnishad622@gmail.com>
2026-09-19 16:49:50 +0530

    fix the pagges err

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -575,7 +575,7 @@ export default function AdminLayout({ children, darkMode, toggleDarkMode, showLa
       active: location.pathname.includes("/resource-manager") || location.pathname.includes("/document/") || location.pathname.includes("/subscription/"),
       module: "Document & Substruction",
       subItems: [
-        { href: "/resource-manager", label: "Insuranace", badge: documents?.length || null, active: location.pathname === "/resource-manager", showFor: ["admin", "user", "HOD"] },
+        { href: "/resource-manager", label: "Insurance", badge: documents?.length || null, active: location.pathname === "/resource-manager", showFor: ["admin", "user", "HOD"] },
         { href: "/document/car-insurance", label: "Car Insurance", active: location.pathname === "/document/car-insurance", showFor: ["admin", "user", "HOD"] },
         { href: "/document/renewal", label: "Document Renewal", badge: docRenewalCount, active: location.pathname === "/document/renewal", showFor: ["admin", "user", "HOD"] },
         { href: "/subscription/renewal", label: "Subscription Renewal", badge: subRenewalCount, active: location.pathname === "/subscription/renewal", showFor: ["admin", "user", "HOD"] },
